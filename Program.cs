9f6660582a2d9ffd0cc51cbc50a3a3ffc9d94eaf
Vladimir <user@MacBook-Air-user.local>
2022-10-10 20:29:58 +0700

    перевели числовое значение в строчное

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного 
+числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+Console.WriteLine("Введите любое число");
+int num = int.Parse(Console.ReadLine()!);
+if (num<100) Console.WriteLine("третьей цифры нет!");
+string str = num.ToString();
+Console.WriteLine();
